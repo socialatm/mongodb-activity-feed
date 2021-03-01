3dc15a1ea6bf3bde2659efceb768d1aedd4ca444
Ray <raypeaslee@gmail.com>
2021-02-28 21:19:43 -0500

    Update setup-tests.js
    we don't use travis-ci currently

--- setup-tests.js
+++ setup-tests.js
@@ -1,6 +1,5 @@
 /* eslint-disable linebreak-style */
 import Mocha from 'mocha'
-
 import config from './src/config'
 import logger from './src/utils/logger'
 import { dropDBs } from './test/utils'
@@ -32,36 +31,26 @@ function wrapMocha(onPrepare, onUnprepare) {
 	}
 }
 
-wrapMocha(
-	async () => {
-		if (!config.database.uri)
-			throw new Error('Missing MongoDB connection string. Check config')
-		if (!config.redis.uri)
-			throw new Error('Missing Redis connection string. Check config')
-		if (
-			!config.database.uri.includes('localhost') &&
-			!config.database.uri.includes('127.0.0.1')
+wrapMocha(async () => {
+	if (!config.database.uri)
+		throw new Error('Missing MongoDB connection string. Check config')
+	if (!config.redis.uri)
+		throw new Error('Missing Redis connection string. Check config')
+	if (
+		!config.database.uri.includes('localhost') &&
+		!config.database.uri.includes('127.0.0.1')
+	)
+		throw new Error(
+			'MongoDB connection string contains non-local address. For safety reasons test suite can only connect to local databases. Check config',
 		)
-			throw new Error(
-				'MongoDB connection string contains non-local address. For safety reasons test suite can only connect to local databases. Check config',
-			)
-		if (
-			!config.redis.uri.includes('localhost') &&
-			!config.redis.uri.includes('127.0.0.1')
+	if (
+		!config.redis.uri.includes('localhost') &&
+		!config.redis.uri.includes('127.0.0.1')
+	)
+		throw new Error(
+			'Redis connection string contains non-local address. For safety reasons test suite can only connect to local databases. Check config',
 		)
-			throw new Error(
-				'Redis connection string contains non-local address. For safety reasons test suite can only connect to local databases. Check config',
-			)
 
-		//XXX: drop all data before running tests
-		await dropDBs()
-	},
-	failures => {
-		//XXX: it seems Travis-ci is having trouble with process wrap-up procedures so lets
-		//     allocate more time before shutting down
-		const timeout = process.env.TRAVIS ? 10000 : 1500
-		logger.info(`Terminating in ${timeout / 1000} seconds`)
-		//XXX: don't care about open connections
-		setTimeout(() => process.exit(failures ? 1 : 0), timeout)
-	},
-)
+	//XXX: drop all data before running tests
+	await dropDBs()
+})
