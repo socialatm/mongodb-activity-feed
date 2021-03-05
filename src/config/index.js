55a2f303bd219d4827fc7bac1d200f361677fa16
Ray <raypeaslee@gmail.com>
2021-03-04 21:09:38 -0500

    db config stuff

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-const config = require('./dev.js')
+const database = { uri: 'mongodb://localhost:27017/test' }
 const _default = {}
-module.exports = Object.assign({ env: process.env.NODE_ENV }, _default, config)
+module.exports = Object.assign(_default, database)
